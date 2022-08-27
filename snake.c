c62d157d16c2923d8d97e03595c29731c84d7180
prasiddha <prasiddha.thapa1@gmail.com>
2022-08-27 18:05:43 +0545

    p11

--- snake.c
+++ snake.c
@@ -11,5 +11,15 @@ int rd()
 }
 int main (void)
 {
-    
+	int i,dice,cur_pos1=0,cur_pos2=0;
+	char ch;
+	while(1)
+	{
+		printf("Snakes: | 25 to 9  | 65 to 40 | 99 to 1  |\nLadder: | 13 to 42 | 60 to 83 | 70 to 93 |\n\n");
+		printf("Choose your option\n");
+		printf("[1] Player 1 plays\n");
+		printf("[2] Player 2 plays\n");
+		printf("[3] Exit\n");
+		scanf("%s",&ch);
+    }
 }
